517f1b83bbd7b3ba4b2ab04e750d6d8ef96f142b
Eden <eden7045@gmail.com>
2022-05-06 14:11:16 +0300

    Revert "cas and start implement linked list"
    This reverts commit bee5e1c24b0cf96d162ebbff2ace4b125abbc857.

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -127,8 +127,6 @@ static uint64 (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
-[SYS_set_cpu] sys_set_cpu,
-[SYS_get_cpu] sys_get_cpu,
 };
 
 void
